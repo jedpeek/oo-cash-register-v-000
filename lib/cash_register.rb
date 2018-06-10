ac9face29746f2ec1f45873ecf866f1097087309
Jed Peek <jedpeek@yahoo.com>
2018-06-10 16:26:51 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ class CashRegister
 
   def apply_discount
     if !!@employee == true
-    @total = (@total*(@discount/100)).to_f
+    @total = (@total*(@discount/100))
     return "After the discount, the total comes to #{@total}"
   elsif @employee == nil
       return "There is no discount to apply"
