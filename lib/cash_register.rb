aa3a85d223e09c73ae7642640e6bc8e0d4b68cc6
Jed Peek <jedpeek@yahoo.com>
2018-06-10 16:04:15 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@
 class CashRegister
   attr_accessor :total, :transaction, :items, :employee
 
-  def initialize(@employee = false)
+  def initialize(employee = false)
     @total = 0
     @items = []
     @employee = true
