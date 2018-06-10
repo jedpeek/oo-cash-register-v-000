0bbaa5dde033c2c60e2a67cd94d27c51979e55d7
Jed Peek <jedpeek@yahoo.com>
2018-06-10 15:44:54 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantities = 1)
-    quantities.times do @items.push(titles)
+    quantities.times {@items << titles}
 
     self.total += price*quantities
   end
